4f9cbf845bba7c462346eeaf3cf42e8edebd894e
AlaaZreekie <alaasama282@gmail.com>
2025-08-25 14:19:43 +0300

    EndDirectivesAndAddBootStrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from "./user/user.component";
 import { DataBindingComponent } from "./data-binding/data-binding.component";
+import { DirectivesComponent } from './directives/directives.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DataBindingComponent],
+  imports: [RouterOutlet, DataBindingComponent, DirectivesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -1,8 +1,10 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { DataBindingComponent } from '../data-binding/data-binding.component';
 
 @Component({
   selector: 'app-directives',
-  imports: [],
+  imports: [NgIf, DataBindingComponent ],
   templateUrl: './directives.component.html',
   styleUrl: './directives.component.scss'
 })
