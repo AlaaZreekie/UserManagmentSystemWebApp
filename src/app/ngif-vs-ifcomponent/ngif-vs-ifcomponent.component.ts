7083c777eae6d86816fa10b69c3bb2ea30ecfa0f
AlaaZreekie <alaasama282@gmail.com>
2025-08-25 14:50:32 +0300

    StartSrtucturealDirectivesAddIfComponent

---
+++ src/app/ngif-vs-ifcomponent/ngif-vs-ifcomponent.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ngif-vs-ifcomponent',
+  imports: [],
+  templateUrl: './ngif-vs-ifcomponent.component.html',
+  styleUrl: './ngif-vs-ifcomponent.component.scss'
+})
+export class NgifVsIfcomponentComponent {
+
+}
