a46710268063af9833efda4990af055c1ec93250
AlaaZreekie <alaasama282@gmail.com>
2025-08-25 15:30:31 +0300

    StartForComponnt

---
+++ src/app/for-component/for-component.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ForComponentComponent } from './for-component.component';
+
+describe('ForComponentComponent', () => {
+  let component: ForComponentComponent;
+  let fixture: ComponentFixture<ForComponentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ForComponentComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ForComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/for-component/for-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-for-component',
+  imports: [],
+  templateUrl: './for-component.component.html',
+  styleUrl: './for-component.component.scss'
+})
+export class ForComponentComponent {
+
+}
