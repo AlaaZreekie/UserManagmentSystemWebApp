753e7550cda415e7a4e7109d264503fcd84bd467
AlaaZreekie <alaasama282@gmail.com>
2025-08-26 16:05:25 +0300

    StartPipes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { BuildInPipesComponent } from './build-in-pipes/build-in-pipes.component';
 
 export const routes: Routes = [
   {path: "", component: UserComponent},
@@ -9,5 +10,6 @@ export const routes: Routes = [
 
   {path: "user", component: UserComponent},
   {path: "data-binding", component: DataBindingComponent},
+  {path: 'build-in-pipes', component: BuildInPipesComponent},
   {path: '**', component: PageNotFoundComponent}
 ];
---
+++ src/app/build-in-pipes/build-in-pipes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-build-in-pipes',
+  imports: [],
+  templateUrl: './build-in-pipes.component.html',
+  styleUrl: './build-in-pipes.component.scss'
+})
+export class BuildInPipesComponent {
+
+}
