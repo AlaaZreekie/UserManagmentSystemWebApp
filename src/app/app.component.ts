b01cad2e31c73b3d9b7fdc01ea967ad6e247cd17
AlaaZreekie <alaasama282@gmail.com>
2025-08-26 12:24:42 +0300

    LinkedSignals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,11 @@ import { ForComponentComponent } from "./for-component/for-component.component";
 import { SwitchComponentComponent } from "./switch-component/switch-component.component";
 import { AttributeDirectivesComponent } from "./attribute-directives/attribute-directives.component";
 import { SignalsComponent } from './signals/signals.component';
+import { LinkedSignalsComponent } from "./linked-signals/linked-signals.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SignalsComponent],
+  imports: [RouterOutlet, SignalsComponent, LinkedSignalsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
