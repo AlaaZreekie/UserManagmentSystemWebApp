46277f53e7a5c4bd75cd279993821e4b9d380821
AlaaZreekie <alaasama282@gmail.com>
2025-08-26 16:44:46 +0300

    KnowBuildInPipes

--- src/app/build-in-pipes/build-in-pipes.component.ts
+++ src/app/build-in-pipes/build-in-pipes.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-build-in-pipes',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './build-in-pipes.component.html',
   styleUrl: './build-in-pipes.component.scss'
 })
 export class BuildInPipesComponent {
-
+  personData = {
+    name : "alaa",
+    age : 22,
+    city: "damas",
+  }
+  items  = of(['apple', 'banana', 'lemon']);
+  currentDate : Date = new Date();
+  angularPipes : string = "use pipes to format data in angular temples";
 }
