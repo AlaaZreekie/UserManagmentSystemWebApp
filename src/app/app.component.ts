8d6b90e3d4b8f556af9ce797de14d30f2e9ea50f
AlaaZreekie <alaasama282@gmail.com>
2025-08-26 15:27:43 +0300

    AddHooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { UserComponent } from "./user/user.component";
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { DataBindingComponent } from "./data-binding/data-binding.component";
 import { DirectivesComponent } from './directives/directives.component';
-import { NgifVsIfcomponentComponent } from "./ngif-vs-ifcomponent/ngif-vs-ifcomponent.component";
 import { ForComponentComponent } from "./for-component/for-component.component";
 import { SwitchComponentComponent } from "./switch-component/switch-component.component";
 import { AttributeDirectivesComponent } from "./attribute-directives/attribute-directives.component";
@@ -12,7 +10,7 @@ import { LinkedSignalsComponent } from "./linked-signals/linked-signals.componen
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import {Router , RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
 
+  constructor(private router : Router){}
+  loginData() {
+    // this.router.navigateByUrl("data-binding");
+    this.router.navigate(['data-binding', ])
+  }
 }
