e8ddef3f89c4f9c7627b182cc066bce2ffa16fe6
AlaaZreekie <alaasama282@gmail.com>
2025-08-26 15:55:16 +0300

    KnowHooks

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import {Router , RouterLink } from '@angular/router';
 
 @Component({
@@ -7,9 +7,53 @@ import {Router , RouterLink } from '@angular/router';
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
-export class UserComponent {
+export class UserComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
-  constructor(private router : Router){}
+  constructor(private router : Router){
+    console.log("Hello world");
+  }
+  ngOnDestroy(): void {
+        //important
+
+    //when destroy component
+    console.log("8. ng destroy called");
+  }
+  ngAfterViewChecked(): void {
+        //important
+
+    //use for ng view has been checked
+    console.log("7. ng view checked called");
+  }
+  ngAfterViewInit(): void {
+        //important
+
+    //use for view child
+    console.log("6. ng view init called");
+  }
+  ngAfterContentChecked(): void {
+    //use for ng content checked
+    console.log("5. ng after content checked called");
+  }
+  ngAfterContentInit(): void {
+    //use for ng content
+    console.log("4. ng After content called");
+  }
+  ngDoCheck(): void {
+    //use every change detection
+    console.log("3. ngDoCheck called");
+  }
+  ngOnInit(): void {
+    //important
+    //this is used for APIs calls
+    console.log("2. ngOnInit called");
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    //use for components input property
+    console.log("1. ngOnChanges called.");
+    console.log("-----------------"+changes+"-----------------------")
+  }
+
+
   loginData() {
     // this.router.navigateByUrl("data-binding");
     this.router.navigate(['data-binding', ])
