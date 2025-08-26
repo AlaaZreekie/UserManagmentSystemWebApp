4788aeb67216033ffe689d728fb7319c245ffb0d
AlaaZreekie <alaasama282@gmail.com>
2025-08-26 13:26:37 +0300

    AddPageNotFound

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   {path: "", component: UserComponent},
@@ -8,5 +9,5 @@ export const routes: Routes = [
 
   {path: "user", component: UserComponent},
   {path: "data-binding", component: DataBindingComponent},
-  {}
+  {path: '**', component: PageNotFoundComponent}
 ];
