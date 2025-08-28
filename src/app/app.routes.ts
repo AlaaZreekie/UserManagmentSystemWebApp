a7438a3c1598fec3afaf32e31c64e5973f1d8b9b
AlaaZreekie <alaasama282@gmail.com>
2025-08-28 11:46:30 +0300

    AddTDF

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { UserComponent } from './user/user.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { BuildInPipesComponent } from './build-in-pipes/build-in-pipes.component';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 
 export const routes: Routes = [
   {path: "", component: UserComponent},
@@ -11,5 +12,6 @@ export const routes: Routes = [
   {path: "user", component: UserComponent},
   {path: "data-binding", component: DataBindingComponent},
   {path: 'build-in-pipes', component: BuildInPipesComponent},
+  {path: 'tdf', component: TemplateDrivenFormComponent},
   {path: '**', component: PageNotFoundComponent}
 ];
